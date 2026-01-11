1b8822a461af3e552eeda7903d80b52510428610
jt00721 <titusgloverj@gmail.com>
2026-01-11 14:05:04 +0000

    feat: implement ContactPicker dropdown with default option

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ name, value, handleChange, contacts }) => {
   return (
-    <></>
+    <>
+      <select onChange={handleChange} value={value} name={name}>
+        <option value="" selected disabled>No Contact Selected</option>
+        {contacts.map((contact) => {
+          return (
+            <option key={contact.name} value={contact.name}>{contact.name}</option>
+          );
+        })}
+      </select>
+    </>
   );
 };
