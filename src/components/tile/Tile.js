f3adc9a8ae41d72f02804150d094603649a5c854
jt00721 <titusgloverj@gmail.com>
2026-01-11 12:52:48 +0000

    feat: implement Tile component for name and description values

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ name, description }) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
+      {Object.values(description).map((val) => {
+        return (
+          <p className="tile">{val}</p>
+        );
+      })}
     </div>
   );
 };
