a7ebe9b68a9a515b8b4aa2be60fd0dcf335b2294
jt00721 <titusgloverj@gmail.com>
2026-01-11 11:51:42 +0000

    feat: implement ContactForm controlled inputs

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,30 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          name="contactName" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
+        />
+        <input 
+          type="tel" 
+          name="contactPhoneNumber" 
+          pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" 
+          value={phone} 
+          onChange={(e) => setPhone(e.target.value)} 
+        />
+        <input 
+          type="email" 
+          name="contactEmail" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+        />
+        <input type="submit" />
+      </form>
+    </>
   );
 };
 
