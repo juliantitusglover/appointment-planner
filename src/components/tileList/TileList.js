cb1d05faf80a368f831be82bec59bb98fd36ce48
jt00721 <titusgloverj@gmail.com>
2026-01-11 12:34:13 +0000

    feat: implement TileList to render generic data tiles

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Tile from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({ dataList }) => {
   return (
     <div>
-      
+      {dataList.map((data, idx) => {
+        const {name, ...rest} = data;
+        return (
+          <Tile name={name} description={rest} key={idx} />
+        );
+      })}
     </div>
   );
 };
